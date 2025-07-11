435c98853272b0b51463f086d95f26641c753b01
jai-atithya <jaiatithya@gmail.com>
2025-07-11 21:33:41 +0530

    Login.jsx - centered login text

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1>Login</h1>
+    </div>
+    </>
   )
 }
