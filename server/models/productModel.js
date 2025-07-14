c69a2e0f9e7d8a171ee20edf3b22c0fbc751dca4
Keerthika <nkeerthika09@gmail.com>
2025-07-15 00:46:21 +0530

    Product model created

---
+++ server/models/productModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        default: "",
+        required: true,
+    },
+    description:{
+        type:String,
+        default:"",
+        required: true,
+    },
+    price:{
+        type: Number,
+        default: 0,
+        rewuired: true,
+    },
+    quantity:{
+        type:Number,
+        default: 1,
+        required: true,
+    },
+    brand:{
+        type:String,
+        default:"",
+        required: true,
+    },
+    category:{
+        type: [String],
+        default:[],
+        required: true,
+    },
+})
