be66242cd43fee65a69b33ba4543d506848fa974
Keerthika <nkeerthika09@gmail.com>
2025-07-14 11:28:50 +0530

    Navbar called in Sell

--- client/src/pages/sell/Sell.jsx
+++ client/src/pages/sell/Sell.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { SellProductForm } from './components/SellProductForm'
+import { Navbar } from '../Navbar'
 
 export const Sell = () => {
   return (
     <>
-      <Navbar />
-      <div>Sell</div>
+      <Navbar/>
       <SellProductForm />
     </>
   )
