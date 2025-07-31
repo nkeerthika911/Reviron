c60fce6873c29965255c83936a12a169da905b57
Keerthika <nkeerthika09@gmail.com>
2025-07-31 22:39:02 +0530

    Product details model changed

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -35,6 +35,11 @@ const productSchema = new mongoose.Schema({
         default: [],
         required: true,
     },
+    images: {
+        type: [String],
+        default: [],
+        required: true,
+    }
 })
 
 module.exports = mongoose.model("Product", productSchema);
