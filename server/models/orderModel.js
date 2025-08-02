72135b1390a685fa77747528d974e4885d40dc36
Keerthika <nkeerthika09@gmail.com>
2025-08-02 09:47:36 +0530

    Product added

---
+++ server/models/orderModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'products',
+      required: true,
+    },
+    status: {
+      type: String,
+      default: 'in processing',
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
