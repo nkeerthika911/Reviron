5d993bb9c42905226e3839049655fd3fa0e2be06
Keerthika <nkeerthika09@gmail.com>
2025-08-05 02:24:22 +0530

    All product form frontend done

---
+++ server/models/userSellProduct.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const userProductSchema = new mongoose.Schema({
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true,
+    },
+    name: {
+        type: String,
+        default: "",
+        required: true,
+    },
+    category: {
+        type: String,
+        default: "",
+        required: true,
+    },
+    condition: {
+        type: String,
+        enum: ["Working", "Not Working"],
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+        required: true,
+    },
+    startPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    endPrice: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("UserProduct", userProductSchema);
