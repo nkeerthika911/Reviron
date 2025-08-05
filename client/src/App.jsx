0ebe6bde48af557ce029d2a75b3974279013ca06
jai-atithya <jaiatithya@gmail.com>
2025-08-05 21:52:51 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import { Profile } from './pages/profile/Profile'
 import { Sell } from './pages/sell/Sell'
 import { Community } from './pages/community/Community'
 import { AdminOrders } from './pages/products/AdminOrders'
-import { WorkerPage } from './pages/products/WorkerPage'
+import { WorkerPage } from './adminpages/employee/WorkerPage'
 import { BuyPage } from './pages/products/BuyPage';
 import { Payment } from './pages/products/components/payment';
 import { ItemView } from './pages/products/ItemView'
@@ -29,10 +29,12 @@ function App() {
           <Route path="/" element={<Login />}></Route>
           <Route path="/products" element={<Products />}></Route>
           <Route path="/product/:productId" element={<ProductDetails/>}></Route>
-          <Route path="/admin/add/products" element={<AdminAddProduct/>}></Route>
           <Route path="/cart" element={<Cart/>}></Route>
           <Route path="/buy" element={<BuyPage/>}></Route>
           <Route path="/contactus" element={<Contactus />}></Route>
+
+          <Route path="/admin/product/add" element={<AdminAddProduct/>}></Route>
+          <Route path="/admin/employee" element={<WorkerPage/>}></Route>
 
           <Route path="/tester" element={<Tester/>}></Route>
           <Route path="/seller" element={<Seller/>}></Route>
@@ -42,7 +44,6 @@ function App() {
           <Route path="/sell" element={<Sell/>}></Route>
           <Route path="/community" element={<Community/>}></Route>
           <Route path="/admin/order" element={<AdminOrders/>}></Route>
-          <Route path="/WorkerPage" element={<WorkerPage/>}></Route>
           <Route path="/payment" element={<Payment/>}></Route>
           <Route path="/ItemView" element={<ItemView/>}></Route>
         </Routes>
