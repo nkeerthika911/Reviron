7aad179cdbb49e611db4f79bd8fa2eb4f75d9b18
Harini <harinisankar1712@gmail.com>
2025-08-08 12:26:18 +0530

    add product

--- client/src/adminpages/products/AdminAddProducts.jsx
+++ client/src/adminpages/products/AdminAddProducts.jsx
@@ -48,7 +48,7 @@ export const AdminAddProduct = () => {
         category: categoryArray
       };
 
-      const res = await axios.post(`${import.meta.env.BASE_URL}/api/products/post`, payload);
+      const res = await axios.post(`${import.meta.env.BASE_URL}https://reviron-1.onrender.com/api/products/favourite/`, payload);
       const productId = res.data.data.data._id;
 
       const formData = new FormData();
